5a0481f5cf719e862570612fd85f5edde1704115
Jones <samamedejones@gmail.com>
2024-04-05 02:36:49 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
-window.open('https://term.ooo/','_blank', 'noopener')
 window.open('https://loldle.net/','_blank', 'noopener')
 window.open('https://worldle.teuteuf.fr/','_blank', 'noopener')
 window.open('https://www.flagle.io/','_blank', 'noopener')
 window.open('https://flagle-game.com/','_blank', 'noopener')
-window.open('https://desafiosdiarios.com/g/hashtag/index.html','_self', 'noopener')
+window.open('https://desafiosdiarios.com/g/hashtag/index.html','_blank', 'noopener')
+window.open('https://term.ooo/','_self', 'noopener')
 
 
