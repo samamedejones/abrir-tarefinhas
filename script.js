741dcfb859e4538e52387987cf68faca088a37e6
Jones <samamedejones@gmail.com>
2024-05-13 18:33:48 -0300

    adição do conexo e narutodle

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 
 window.open('https://loldle.net/','_blank', 'noopener')
+window.open('https://narutodle.net/classic','_blank', 'noopener')
 window.open('https://worldle.teuteuf.fr/','_blank', 'noopener')
 window.open('https://www.flagle.io/','_blank', 'noopener')
 window.open('https://flagle-game.com/','_blank', 'noopener')
 window.open('https://desafiosdiarios.com/g/hashtag/index.html','_blank', 'noopener')
+window.open('https://conexo.ws/pt/','_blank', 'noopener')
 window.open('https://term.ooo/','_self', 'noopener')
 
 
